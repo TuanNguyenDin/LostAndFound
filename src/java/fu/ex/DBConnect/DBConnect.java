70bdfa4ca75b0f732dfa62b551b5b2253b41608d
Đinh Tuân Nguyễn <dinhtunann161@gmail.com>
2024-02-26 13:09:19 +0700

    update DBConnect

--- src/java/fu/ex/DBConnect/DBConnect.java
+++ src/java/fu/ex/DBConnect/DBConnect.java
@@ -7,29 +7,34 @@ package fu.ex.DBConnect;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 /**
  *
  * @author ADMIN
  */
 public class DBConnect {
-//    public static void DBconnect(){
-//         try {
-//            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo", "root", "23072001");
-//            System.out.println("Connected With the database successfully"); //Message after successful connection
-//        } catch (SQLException e) {
-//            System.out.println("Error while connecting to the database"); //Message if something goes wrong while conneting to the database
-//        }
-//    }
-    public Connection getConnection() throws Exception {
-        String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        return DriverManager.getConnection(url, userID, password);
-    }
 
-    private final String serverName = "localhost";
-    private final String portNumber = "1433";
-    private final String dbName = "LnF";
-    private final String userID = "sa";
-    private final String password = "sa";
+    public Connection getConnection() throws Exception, NamingException {
+        Connection connection = null;
+        Context ctx = new InitialContext();
+        Context envContext = (Context) ctx.lookup("java:comp/env");
+        DataSource ds = (DataSource) envContext.lookup("DataText");
+        connection = ds.getConnection();
+        return connection;
+    }
+//    public Connection getConnection() throws Exception {
+//        String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
+//        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//        return DriverManager.getConnection(url, userID, password);
+//    }
+//
+//    private final String serverName = "localhost";
+//    private final String portNumber = "1433";
+//    private final String dbName = "LnF";
+//    private final String userID = "sa";
+//    private final String password = "1234";
 }
